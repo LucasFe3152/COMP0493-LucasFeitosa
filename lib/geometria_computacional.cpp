36b34c7e4c0f05b9f929bd05996fc59079e9e273
LucasFe3152 <167145993+LucasFe3152@users.noreply.github.com>
2025-02-16 18:01:25 -0300

    add class Line

--- lib/geometria_computacional.cpp
+++ lib/geometria_computacional.cpp
@@ -1,20 +1,49 @@
 #include "header.h"
 
-class Ponto2D {
+class Point2D {
     public:
         double x;
         double y;
 
-    Ponto2D() : x(0.0), y(0.0) {}
-    Ponto2D(double x, double y) : x(x), y(y) {}
+    Point2D() : x(0.0), y(0.0) {}
+    Point2D(double x, double y) : x(x), y(y) {}
 };
 
-class Ponto3D {
+class Point3D {
     public:
         double x;
         double y;
         double z;
 
-    Ponto3D() : x(0.0), y(0.0), z(0.0) {}
-    Ponto3D(double x, double y, double z) : x(x), y(y), z(z) {}
+    Point3D() : x(0.0), y(0.0), z(0.0) {}
+    Point3D(double x, double y, double z) : x(x), y(y), z(z) {}
 };
+
+class Line {
+    public:
+        double m;
+        Point2D p;
+    
+    Line(Point2D p1, Point2D p2) : m((p2.y - p1.y) / (p2.x - p1.x)), p(p1) {}
+
+    bool belongs_in_line(Point2D pn) {
+        double a = (pn.y - p.y) / (pn.x - p.x);
+        return (a == m);
+    }
+
+    double get_y(double x) {
+        return (m * (x-p.x)) + p.y;
+    }
+
+    double get_x(double y) {
+        return (y-p.y+m*p.x) / m;
+    }
+};
+
+double distance_between_points2D(Point2D p1, Point2D p2) {
+    return sqrt(pow((p1.x - p2.x), 2) + pow((p1.y - p2.y), 2));
+}
+
+double distance_between_points3D(Point3D p1, Point3D p2) {
+    return sqrt(pow((p1.x - p2.x), 2) + pow((p1.y - p2.y), 2) + pow((p1.z - p2.z), 2));
+}
