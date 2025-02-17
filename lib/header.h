cb5aa295508528579f8ba118b032d88734029667
LucasFe3152 <167145993+LucasFe3152@users.noreply.github.com>
2025-02-16 21:42:21 -0300

    update header and deleted main

--- lib/header.h
+++ lib/header.h
@@ -34,7 +34,7 @@ int binary_search(vector<T>& input, T key);
 string longest_common_prefix(vector<string>& input);
 
 // algoritmo guloso
-float mochila_fracionaria(float capacity, vector<float> values, vector<float> weights);
+float mochila_fracionaria(float capacity, vector<float>& values, vector<float>& weights);
 vector<int>& problema_troco(int exchange, vector<int>& coins);
 
 
@@ -49,6 +49,7 @@ double distance_between_points3D(Point3D p1, Point3D p2);
 double distance_to_point(Point2D p, Line l);
 
 //grafos
-void dfs(int u, vector<int>& adj, vector<bool>& visited);
+void dfs(int u, unordered_map<int, vector<int>>& adj, vector<bool>& visited);
+void bfs(int u, unordered_map<int, vector<int>>& adj, vector<bool>& visited);
 
 #endif
